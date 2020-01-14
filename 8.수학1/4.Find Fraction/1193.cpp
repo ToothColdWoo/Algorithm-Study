79b59f723466e8068dc902240c0d50f37c4ff862
ToothColdWoo <upup1810@naver.com>
2020-01-14 23:08:44 +0900

    Solve 1193

--- 8.수학1/4.Find Fraction/1193.cpp
+++ 8.수학1/4.Find Fraction/1193.cpp
@@ -1,14 +1,19 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int num;
-    scanf("%d",&num);
-    int cnt = 1;
-    int sum = 1;
-    if(num == 1){
-        printf("1/1");
-    }
-    while(1){
-        if(num > )
-    }
+int main() {
+	int num;
+	scanf("%d", &num);
+	int diagonal = 1;
+	int sum = 1;
+	while (sum < num) {
+		diagonal++;
+		sum = sum + diagonal;
+	}
+	int result = sum - num + 1;
+	if (diagonal % 2 == 0) {
+		printf("%d/%d", diagonal + 1 - result, result);
+	}
+	else {
+		printf("%d/%d", result, diagonal + 1 - result);
+	}
 }
