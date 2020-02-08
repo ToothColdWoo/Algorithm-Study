5b5bb43e7037d04b9137bc9af47fdf991cadec3d
ToothColdWoo <upup1810@naver.com>
2020-02-08 21:27:51 +0900

    Resolve 2108

--- 12.정렬/4.Statistics/2108.cpp
+++ 12.정렬/4.Statistics/2108.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
+#include<algorithm>
 #include <vector>
-#include <cmath>
-#include <algorithm>
 using namespace std;
-
-int main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    int N, temp, mean, mode = 0, min, max = 0;
-    bool isSecond = false;
-    cin>>N;
-    vector<int> vec(N);
-    vector<int> vec2(8001, 0);
-    for(int i = 0; i < N; i ++){
-        cin>>vec[i];
-        mean += vec[i];
-        temp = (vec[i] <= 0) ? abs(vec[i]) : vec[i] + 4000;
-        vec2[temp]++;
-        if(vec2[temp] > max)
-            max = vec2[temp];
+int N;
+int arr[500000];
+int tmp[8001];
+void ArithmeticMean(){
+    double sum = 0 ;
+    for(int i = 0; i < N; i++){
+        sum += arr[i];
     }
-    sort(vec.begin(), vec.end());
-
-    for(int i = -4000; i < 4001; i++){
-        temp = i <= 0 ? abs(i) : i+4000;
-        if(vec2[temp] == max){
-            mode = i;
-            if(isSecond)
-                break;
-            isSecond = true;
+    if(sum > 0){
+        printf("%d\n",(int)(sum/N + 0.5));
+    }
+    else{
+        printf("%d\n",(int)(sum/N - 0.5));
+    }
+}
+void Median() {
+    sort(arr,arr+N);
+    printf("%d\n",arr[N/2]);
+}
+void Mode() {
+    int num = -1;
+    int max = 0;
+    int result;
+    for(int i = 0; i <= 8000; i++){
+        if(max < tmp[i]){
+            max = tmp[i];
+            num = i;
         }
     }
+    for(int i=0; i <= 8000; i++){
+        if(i > num && max == tmp[i]){
+            result = i - 4000;
+            break;
+        } else
+            result = num - 4000;
+    }
+    printf("%d\n",result);
+}
+void Range() {
+    printf("%d\n",arr[N-1] - arr[0]);
+}
 
-    cout<<round(mean/(double)N)<<'\n';
-    cout<<vec[round(N/2)]<<'\n';
-    cout<<mode<<'\n';
-    min = vec[0];
-    max = vec[vec.size()-1];
-    cout<<max - min<<'\n';
+int main(void) {
+    scanf("%d",&N);
+    for(int i = 0 ; i < N; i++){
+        scanf("%d",&arr[i]);
+        tmp[arr[i]+4000]++;
+    }
+    ArithmeticMean();
+    Median();
+    Mode();
+    Range();
 }
