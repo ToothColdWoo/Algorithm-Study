3ad65a62a6b1918fbfa940fc9e5816042b13248f
ToothColdWoo <upup1810@naver.com>
2020-02-09 20:52:23 +0900

    Solve 11650

---
+++ 12.정렬/6.Sort Coordinate/11650.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    int a, b,N;
+    vector<pair<int,int>> v;
+    scanf("%d",&N);
+    for(int i = 0 ; i < N; i++){
+        scanf("%d %d",&a, &b);
+        v.push_back(make_pair(a,b));
+    }
+
+    sort(v.begin(),v.end());
+    vector<pair<int,int>> ::iterator iter;
+    for(iter=v.begin();iter!=v.end();iter++){
+        cout<<iter->first<<' '<<iter->second<<'\n';
+    }
+}
