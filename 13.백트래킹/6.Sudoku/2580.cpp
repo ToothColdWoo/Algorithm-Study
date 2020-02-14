d54664831a2fbff433c684be7824571b6701ed6f
ToothColdWoo <upup1810@naver.com>
2020-02-14 22:14:52 +0900

    Attempting Sudoku

---
+++ 13.백트래킹/6.Sudoku/2580.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#define MAX 9
+int Sudoku[MAX][MAX];
+bool Row[MAX][MAX];
+bool Col[MAX][MAX];
+
+int cnt;
+bool Row(){
+
+}
+void DFS(int n){
+    if(n == cnt){
+        return;
+    }
+    for(int i = 0 ; i< MAX;i++){
+
+    }
+}
+int main() {
+    for(int i = 0; i < MAX; i++) {
+        for(int j = 0; j < MAX; j++) {
+            cin >> Sudoku[i][j];
+            if(Sudoku[i][j] == 0)
+                cnt++;
+        }
+    }
+    DFS(0)
+    for(int i = 0; i < MAX; i++) {
+        for(int j = 0; j < MAX; j++) {
+            cin >> Sudoku[i][j];
+        }
+    }
+}
