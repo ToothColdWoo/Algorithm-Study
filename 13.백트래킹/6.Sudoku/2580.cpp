310def56951d845477be43a25feececb71d24b05
ToothColdWoo <upup1810@naver.com>
2020-02-17 21:24:58 +0900

    Solve 2580 with https://yabmoons.tistory.com/88

--- 13.백트래킹/6.Sudoku/2580.cpp
+++ 13.백트래킹/6.Sudoku/2580.cpp
@@ -1,39 +1,96 @@
 #include<iostream>
-using namespace std;
+
+#define endl "\n"
 #define MAX 9
-int Sudoku[MAX][MAX];
+using namespace std;
+
+int MAP[MAX][MAX];
 bool Row[MAX][MAX];
 bool Col[MAX][MAX];
+bool Square[MAX][MAX];
 
-int cnt;
-bool Row(){
-
-}
-
-bool Col() {
-    
-}
-
-void DFS(int n){
-    if(n == cnt){
-        return;
-    }
-    for(int i = 0 ; i< MAX;i++){
-
-    }
-}
-int main() {
-    for(int i = 0; i < MAX; i++) {
-        for(int j = 0; j < MAX; j++) {
-            cin >> Sudoku[i][j];
-            if(Sudoku[i][j] == 0)
-                cnt++;
-        }
-    }
-    DFS(0)
-    for(int i = 0; i < MAX; i++) {
-        for(int j = 0; j < MAX; j++) {
-            cin >> Sudoku[i][j];
+void Input()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            cin >> MAP[i][j];
+            if (MAP[i][j] != 0)
+            {
+                Row[i][MAP[i][j]] = true;
+                Col[j][MAP[i][j]] = true;
+                Square[(i / 3) * 3 + (j / 3)][MAP[i][j]] = true;
+            }
         }
     }
 }
+
+void Print()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            cout << MAP[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DFS(int Cnt)
+{
+    int x = Cnt / MAX;    // x 좌표
+    int y = Cnt % MAX;    // y 좌표
+
+    if (Cnt == 81)
+    {
+        Print();
+        exit(0);
+    }
+
+    if (MAP[x][y] == 0)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            if (Row[x][i] == false && Col[y][i] == false && Square[(x / 3) * 3 + (y / 3)][i] == false)
+            {
+                Row[x][i] = true;
+                Col[y][i] = true;
+                Square[(x / 3) * 3 + (y / 3)][i] = true;
+                MAP[x][y] = i;
+                DFS(Cnt + 1);
+                MAP[x][y] = 0;
+                Row[x][i] = false;
+                Col[y][i] = false;
+                Square[(x / 3) * 3 + (y / 3)][i] = false;
+            }
+        }
+    }
+    else DFS(Cnt + 1);
+}
+
+void Solution()
+{
+    DFS(0);
+}
+
+void Solve()
+{
+    Input();
+    Solution();
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Solve();
+
+    return 0;
+}
+
+
+// https://yabmoons.tistory.com/88
