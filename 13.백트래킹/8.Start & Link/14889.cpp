6a3eb40780b12f08ce06695b285a0e70817789c4
ToothColdWoo <upup1810@naver.com>
2020-02-21 22:21:23 +0900

    Solve 14889

--- 13.백트래킹/8.Start & Link/14889.cpp
+++ 13.백트래킹/8.Start & Link/14889.cpp
@@ -1,32 +1,56 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
 #define MAX 21
 using namespace std;
+
 int N;
 int arr[MAX][MAX];
+vector<int> Start;
+vector<int> Link;
+int ans = 123456789;
 
-void SetTeam(){
+int DFS(int cnt) {
+    if(cnt >= N){
+        if(Start.size() == Link.size()) {
+            int StartSum = 0 ;
+            int LinkSum = 0 ;
 
-}
-void DFS(int x, int y) {
-    SetTeam();
-    if(cnt == N){
-        return;
-    }
-    for(int i =0  ; i < N; i++) {
-        for(int j = 0 ; j < N; j++) {
-            if(i == j ){
-                continue;
+            for(int i= 0; i < Start.size(); i++) {
+                for(int j = i+1; j < Start.size(); j++) {
+                    StartSum += arr[Start[i]][Start[j]] + arr[Start[j]][Start[i]];
+                }
             }
-            
+
+            for(int i = 0; i < Link.size(); i++) {
+                for(int j = i + 1; j < Link.size(); j++) {
+                    LinkSum += arr[Link[i]][Link[j]] + arr[Link[j]][Link[i]];
+                }
+            }
+            ans = min(ans,abs(StartSum - LinkSum));
         }
+        return 0;
     }
+    Start.push_back(cnt);
+    DFS(cnt + 1);
+    Start.pop_back();
+
+    Link.push_back(cnt);
+    DFS(cnt + 1);
+    Link.pop_back();
+
+    return 0;
+
 }
 int main() {
     cin >> N;
-    for(int i = 1 ; i <=N; i++) {
-        for(int j = 1; j <= N; j++) {
+    for(int i = 0 ; i <N; i++) {
+        for(int j = 0; j < N; j++) {
             cin >> arr[i][j];
         }
     }
-    DFS(0,0);
+    DFS(0);
+    cout << ans;
+    return 0;
 }
